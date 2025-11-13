da7e12c56c95f7e3ec6b8608fcf74c9f2ae7f089
LMorlok <94486446+LMorlok@users.noreply.github.com>
2025-11-13 12:58:18 +0000

    Deploying to gh-pages from @ LMorlok/lmorlok.github.io@6b963e2050e01d6bf3c98d70292f3b0830432e98 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "LMorlok/lucamorlok.github.io",
+    "data-repo": "LMorlok/lmorlok.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
